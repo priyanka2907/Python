ac99df47aefebd591a565c7a8bd216e922ead2eb
Priyanka Jamdar <33025640+priyanka2907@users.noreply.github.com>
2021-12-22 13:09:50 +0530

    Update and rename Assignment1_4.py to Range.py

--- Range.py
+++ Range.py
@@ -1,6 +1,6 @@
 def main():
     for i in range(5):
-        print("Marvellous");
+        print("Python");
         
 
 if __name__== "__main__":
@@ -9,10 +9,10 @@ if __name__== "__main__":
 ###########Output
 
 #C:\Users\Abhi\Desktop\Python\Assignment1>python assignment1_4.py
-#Marvellous
-#Marvellous
-#Marvellous
-#Marvellous
-#Marvellous
+#Python
+#Python
+#Python
+#Python
+#Python
 
 #C:\Users\Abhi\Desktop\Python\Assignment1>
